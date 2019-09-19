f18524398eebfd761860803970eaf48bd67ec7ee
violabai <somnusdoudou@gmail.com>
2019-09-19 14:09:38 -0500

    ..

--- p5/week5bugbusters/sketch.js
+++ p5/week5bugbusters/sketch.js
@@ -1,41 +1,40 @@
-var = myState;
-
-
+var myState ;
 function Setup() {
   // put setup code here
-  createCanvas(800; 800);
+  createCanvas(800, 800);
 }
 
 
 function draw() {
-    switch (myState) {
+  switch (myState) {
 
     case 0:
-    background(255,0,0)
-    fill('white');
-    textSize(100);
-    text("case 0", 100, 100);
-    break;
+      background(255, 0, 0)
+      fill('white');
+      textSize(100);
+      text("case 0", 100, 100);
+      break;
 
     case 1:
       background(0, 255, 0);
-     fill('black');
-     textSize(100);
+      fill('black');
+      textSize(100);
       text("case 1", 100, 100);
       break;
 
-  case 2:
-    background(0, 0, 255);
-   fill ('gold');
-  textSize(100);
-       text("case 2", 100, 100);
-    break;
+    case 2:
+      background(0, 0, 255);
+      fill('gold');
+      textSize(100);
+      text("case 2", 100, 100);
+      break;
+  }
 }
-}
-function mouseReleased(){
- myState++ ;
- if (myState > 2){
-   myState = 0 ;
- }
- myState = (myState + 1) % 3 ;
+
+function mouseReleased() {
+  myState++;
+  if (myState > 2) {
+    myState = 0;
+  }
+  myState = (myState + 1) % 3;
 }

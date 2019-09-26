9babd9003a49e10e0818588d6aebb8c11b491773
violabai <somnusdoudou@gmail.com>
2019-09-26 14:24:26 -0500

    .

--- p5/week6joke2/sketch.js
+++ p5/week6joke2/sketch.js
@@ -1,5 +1,5 @@
 var myState = 0;
-
+var myTimer = 0;
 function setup() {
   // put setup code here
   createCanvas(800, 800);
@@ -12,6 +12,10 @@ function draw() {
       background('blue');
       text("有一只公鹿 他走着走着 越走越快", 100, 100);
       fill('black');
+      if (myTimer >=200){
+        myState = 1;
+        myTimer = 0;
+      }
       break;
 
     case 1:
@@ -22,10 +26,4 @@ function draw() {
   }
 }
 
-  function mouseReleased() {
-    myState = myState + 1
-    if (myState > 2){
-      myState = 0;
-    }
-
-  }
+  

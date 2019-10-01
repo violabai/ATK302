d77223e840da1c50959fe4a96c4ce60972848925
violabai <somnusdoudou@gmail.com>
2019-10-01 18:59:49 -0500

    /

--- p5/week6stoplight/sketch.js
+++ p5/week6stoplight/sketch.js
@@ -5,54 +5,52 @@ function setup() {
   // put setup code here
   createCanvas(800, 800);
   rectMode(CENTER);
-  ellipsMode(CENTER);
+  ellipseMode(CENTER);
 }
 
 function draw() {
   // put drawing code here
- fill('blue');
- rect(width/2,height/2,150,500);
+  fill('blue');
+  rect(width / 2, height / 2, 150, 500);
 
- switch (myState){
- case 0:
- fill ('red');
- ellipse(with/2,250,100,100);
- fill('grey');
- ellipse(with/2,height/2,100,100);
- fill('grey');
- ellipse(with/2,550,100,100);
- myTimer++;
- if (myTimer >=200);{
-   myState ++;
-   myTimer = 0;
- }
- break;
+  switch (myState) {
+    case 0:
+      fill('red');
+      ellipse(width / 2, 250, 100, 100);
+      myTimer++;
+      if (myTimer >= 200) {
+        myState ++;
+        myTimer = 0;
+      }
+      fill('grey');
+      ellipse(width / 2, height / 2, 100, 100);
+      fill('grey');
+      ellipse(width / 2, 550, 100, 100);
 
- case 1:
- fill('yellow');
- ellipse(with/2,height/2,100,100);
- fill('grey');
- ellipse(with/2,250,100,100);
- fill('grey');
- ellipse(with/2,550,100,100);
- myTimer++;
- if (myTimer >=200);{
-   myState ++;
-   myTimer = 0;
- }
- break;
+      break;
 
- case 2:
- fill('green');
- ellipse(with/2,550,100,100);
- fill('grey');
- ellipse(with/2,height/2,100,100);
- fill('grey');
- ellipse(with/2,250,100,100);
- break;
+    case 1:
+      fill('yellow');
+      ellipse(width / 2, height / 2, 100, 100);
+      myTimer++;
+      if (myTimer >= 200) {
+        myState++;
+        myTimer = 0;
+      }
+      fill('grey');
+      ellipse(width / 2, 250, 100, 100);
+      fill('grey');
+      ellipse(width / 2, 550, 100, 100);
+      break;
 
+    case 2:
+      fill('green');
+      ellipse(width / 2, 550, 100, 100);
+      fill('grey');
+      ellipse(width / 2, height / 2, 100, 100);
+      fill('grey');
+      ellipse(width / 2, 250, 100, 100);
+      break;
 
-
-
-
+}
 }

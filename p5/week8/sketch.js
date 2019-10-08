6b7859344755c7876a861252a6b95e5d1d3b4ed1
violabai <somnusdoudou@gmail.com>
2019-10-08 14:33:02 -0500

    。

--- p5/week8/sketch.js
+++ p5/week8/sketch.js
@@ -1,21 +1,40 @@
-var = myCar1 ; //declare it
-
+var myCar1; //declare it
+var myCar2;
 function setup() {
   createCanvas(800, 800);
-  myCar1 = new Car() ; //spawn 1 car
+  myCar1 = new Car(); //spawn 1 car
+  myCar2 = new Car();
 }
 
 function draw() {
   // put drawing code here
-background(100);
-myCar1.display();
-}
-function Car(){
-  //attributes
-this.x = 100 ;
+  background(100);
 
-  //methods
-this.display = function(){
-  rect(this.x, 100, 100, 50) ;
+  myCar1.display();
+  myCar1.drive();
+
+  myCar2.display();
+  myCar2.drive();
 }
+
+function Car() {
+  //attributes
+  this.x = random(width);
+  this.y = random(height);
+  this.r = random(255) ;
+  this.g = random(255) ;
+  this.b = random(255) ;
+  this.vel = random(5,10);
+  //methods
+  this.display = function() {
+    fill(this.r,this.g, this.b);
+    rect(this.x, this.y, 100, 50);
+}
+  this.drive = function (){
+      this.x = this.x + this.vel;
+      if (this.x > width) {
+         this.x = 0;
+      }
+    }
+
 }
---
+++ p5/week8lterate/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+  // put setup code here
+  createCanvas(500, 500);
+  var names =[];
+  names[0] = "chewie";
+  names[1] = "leia";
+  names[2] = "luke";
+  names[3] = "kyle ren";
+
+  for (var i = 0; i < names.length; i++){
+    console.log(names[i]);
+  }
+
+  var dwarves = [] ;  // creating (declaring) a new array with 7 Strings in it
+dwarves[0] = "dopey" ;
+dwarves[1] = "doc" ;
+dwarves[2] = "grumpy" ;
+dwarves[3] = "happy" ;
+dwarves[4] = "sleepy" ;
+dwarves[5] = "sneezy" ;
+dwarves[6] = "bashful" ;
+
+for (var i = 0; i < dwarves.length; i++){
+  console.log(dwarves[i]);
+}
+
+}
+
+function draw() {
+  // put drawing code here
+
+}

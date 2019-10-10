a548da76af0822177f6f0f62471f7b3826b9036e
violabai <somnusdoudou@gmail.com>
2019-10-10 14:48:45 -0500

    。

--- p5/week8-2/sketch.js
+++ p5/week8-2/sketch.js
@@ -1,43 +1,58 @@
-var myCar1; //declare it
-var myCar2;
+var cars = [] ;//declare it
+var frogPos;
+
 function setup() {
   createCanvas(800, 800);
-  myCar1 = new Car(); //spawn 1 car
-  myCar2 = new Car();
+  for (var i = 0; i < 40; i++) {
+    cars.push(new Car());
+  }
+  frogPos = createVector(width/2, height-80);
 }
 
 function draw() {
   // put drawing code here
   background(100);
-
-  myCar1.display();
-  myCar1.drive();
-
-  myCar2.display();
-  myCar2.drive();
+  for (var i = 0; i < cars.length; i++) {
+  cars[i].display();
+  cars[i].drive();
+  if (cars[i].pos.dist(frogPos) < 50){
+    cars.splice(i, 1);
+  }
 }
+  fill("green")
+  ellipse(frogPos.x, frogPos.y,60, 60);
+  checkForKeys();
+  }
 
 function Car() {
   //attributes
   this.x = random(width);
   this.y = random(height);
-  this.r = random(255) ;
-  this.g = random(255) ;
-  this.b = random(255) ;
+  this.r = random(255);
+  this.g = random(255);
+  this.b = random(255);
 
-  this.vel = createVector(random(-5, 5),random(-5, 5));
+  this.vel = createVector(random(-5, 5), random(-5, 5));
   this.pos = createVector(100, 100);
 
   this.display = function() {
-    fill(this.r,this.g, this.b);
+    fill(this.r, this.g, this.b);
     rect(this.pos.x, this.pos.y, 100, 50);
+  }
+  this.drive = function() {
+    this.pos.add(this.vel);
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+  }
+
 }
-  this.drive = function (){
-    this.pos. add (this.vel);
-      if (this.pos.x > width) this.pos.x = 0;
-      if (this.pos.x < 0) this.pos.x = width;
-      if (this.pos.x > height) this.pos.y = 0;
-      if (this.pos.x < 0 this.pos.y = height;
-    }
+function checkForKeys(){
+  if (keyIsDown(LEFT_ARROW)) frogPos.x = frogPos.x - 5;
+  if (keyIsDown(RIGHT_ARROW)) frogPos.x = frogPos.x + 5;
+  if (keyIsDown(UP_ARROW)) frogPos.x = frogPos.x - 5;
+  if (keyIsDown(DOWN_ARROW)) frogPos.x = frogPos.x + 5;
+
 
 }

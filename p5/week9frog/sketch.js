062f958d59a1488004ad1ab4c2fde25fa3652201
violabai <somnusdoudou@gmail.com>
2019-10-17 13:44:24 -0500

    。

---
+++ p5/week9frog/sketch.js
@@ -0,0 +1,66 @@
+var cars = [];
+var frogPos ;
+
+function setup() {
+
+  createCanvas(800, 800);
+  for (var i = 0; i < 40; i++) {
+    cars.push(new Car());
+  }
+  frogPos = createVector(width/2, height-80) ;
+  rectMode(CENTER) ;
+  ellipseMode(CENTER) ;
+}
+
+function draw() {
+  background(100);
+  for (var i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].drive();
+    if (cars[i].pos.dist(frogPos) < 50) {
+      cars.splice(i, 1) ;
+    }
+  }
+
+  // draw the frog
+  fill('green') ;
+  ellipse(frogPos.x, frogPos.y, 60, 60) ;
+  checkForKeys() ;
+}
+
+// car class!!
+function Car() {
+  // attributes
+  this.pos = createVector(100, 100);
+  this.vel = createVector(random(-5, 5), random(-5, 5));
+  this.r = random(255);
+  this.g = random(255);
+  this.b = random(255);
+
+
+  // methods
+  this.display = function() {
+    fill(this.r, this.g, this.b);
+    rect(this.pos.x, this.pos.y, 100, 50);
+  }
+
+  this.drive = function() {
+    this.pos.add(this.vel);
+
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+
+  }
+
+}
+
+
+function checkForKeys() {
+  if (keyIsDown(LEFT_ARROW)) frogPos.x = frogPos.x - 5;
+  if (keyIsDown(RIGHT_ARROW)) frogPos.x = frogPos.x + 5;
+  if (keyIsDown(UP_ARROW)) frogPos.y = frogPos.y - 5;
+  if (keyIsDown(DOWN_ARROW)) frogPos.y = frogPos.y + 5;
+
+}
